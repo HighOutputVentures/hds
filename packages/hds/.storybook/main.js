265e540dbe514f7510b6fbd38984fa2c2b2afa06
Niel Berongoy <niel_berongoy@yahoo.com>
2022-10-19 15:54:41 +0800

    chore(hds): relocate packages source into one

--- packages/hds/.storybook/main.js
+++ packages/hds/.storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)', '../src/**/*.stories.mdx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chakra-ui/storybook-addon',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
+    '@storybook/mdx2-csf'
   ],
+  features: {
+    previewMdx2: true, // 👈 MDX 2 enabled here
+  },
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
