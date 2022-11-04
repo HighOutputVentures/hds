0caa58792544319c3a21f702cda0dbcdd7f36214
Niel Berongoy <niel_berongoy@yahoo.com>
2022-11-04 16:34:50 +0800

    chore: fix build checking error

--- packages/spinner/tsdx.config.js
+++ packages/spinner/tsdx.config.js
@@ -1,16 +1,6 @@
-const css = require('rollup-plugin-import-css');
-const url = require('rollup-plugin-url');
-
 module.exports = {
   rollup(config, options) {
-    config.plugins = [
-      ...config.plugins,
-      css(),
-      url({
-        include: ['**/*.ttf', '**/*.css'],
-        limit: Infinity,
-      }),
-    ];
+    config.plugins = [...config.plugins];
 
     return config;
   },
