ec30b1cc2715d64928286c183f7b2ecee246261b
Niel Berongoy <niel_berongoy@yahoo.com>
2022-11-04 10:39:59 +0800

    refactor font remove font source and replace with google font

--- packages/hds/src/components/ThemeProvider.tsx
+++ packages/hds/src/components/ThemeProvider.tsx
@@ -1,14 +1,15 @@
 import { ChakraProvider, ChakraProviderProps } from '@chakra-ui/react';
 import React, { FC } from 'react';
-import '@fontsource/inter';
 
 import { extendTheme } from '../utils/theme.utils';
+import Fonts from './Fonts';
 
 export interface ThemeProviderProps extends ChakraProviderProps {}
 
 const ThemeProvider: FC<ThemeProviderProps> = ({ children, ...props }) => {
   return (
     <ChakraProvider {...props} theme={extendTheme(props.theme)}>
+      <Fonts />
       {children}
     </ChakraProvider>
   );
