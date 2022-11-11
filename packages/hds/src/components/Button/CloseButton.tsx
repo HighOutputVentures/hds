29482bbd12a8bf83f120e10a6e65f6e73037bfc7
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-11-11 12:13:23 +0800

    refactor: removed custom styling of buttons

--- packages/hds/src/components/Button/CloseButton.tsx
+++ packages/hds/src/components/Button/CloseButton.tsx
@@ -1,37 +1,26 @@
-import { CloseIcon, IconProps } from '@chakra-ui/icons';
-import {
-  Button,
-  ButtonProps,
-  ComponentWithAs,
-  Icon,
-  ThemingProps,
-} from '@chakra-ui/react';
+import { CloseIcon } from '@chakra-ui/icons';
+import { Button, Icon, ThemingProps } from '@chakra-ui/react';
 import React from 'react';
 
-type WithoutChildren<T> = Omit<T, 'children'>;
-export interface CloseButtonPartProps {
-  button?: WithoutChildren<ButtonProps>;
-}
 export interface CloseButtonProps {
-  icon?: ComponentWithAs<'svg', IconProps>;
   onClicked?: () => void;
   variant?: ThemingProps<'Button'>['variant'];
   size?: ThemingProps<'Button'>['size'];
-  partProps?: Partial<CloseButtonPartProps>;
+  disabled?: boolean;
 }
 
 const CloseButton = (props: Omit<CloseButtonProps, 'children'>) => {
-  const { partProps, icon, variant, size, onClicked } = props;
+  const { disabled, variant, size, onClicked } = props;
 
   return (
     <Button
       size={size ?? 'button-close-sm'}
       variant={variant ?? 'solid-close-btn'}
-      {...partProps?.button}
+      isDisabled={disabled}
       data-testid="close.btn"
       onClick={onClicked}
     >
-      <Icon as={icon ?? CloseIcon} />
+      <Icon as={CloseIcon} />
     </Button>
   );
 };
