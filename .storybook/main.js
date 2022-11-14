efd0e548afb93e755c8b0414007c63c7012c751b
Niel Berongoy <niel_berongoy@yahoo.com>
2022-11-14 08:15:29 +0800

    fix: fix version and build issue in storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,22 @@
 module.exports = {
-  stories: ['../packages/**/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../packages/**/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@chakra-ui/storybook-addon',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/mdx2-csf'
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/mdx2-csf",
   ],
   features: {
     previewMdx2: true, // 👈 MDX 2 enabled here
+    postcss: false,
+    emotionAlias: false,
   },
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  },
-  features: {
-    emotionAlias: false,
   },
 };
