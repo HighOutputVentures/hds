4b7f3318098505d10442a8af18d7130c32cf221d
John Christopher Go <go.jchristopher@gmail.com>
2022-11-16 16:11:32 +0800

    feat (hds-utilities): added For and Show Component

---
+++ packages/utilities/src/components/Show/Show.stories.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+import { useArgs } from '@storybook/addons';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import Show from './Show';
+
+export default {
+  title: 'Utilities/Show',
+  component: Show,
+} as ComponentMeta<typeof Show>;
+
+const Template: ComponentStory<typeof Show> = defaultArgs => {
+  const [args, setArgs] = useArgs();
+
+  const toggle = () => {
+    setArgs({ ...args, when: !args.when });
+  };
+
+  return (
+    <Box p={4}>
+      <Show {...defaultArgs}>
+        <Text>Hello World!!</Text>
+      </Show>
+
+      <Button mt={4} onClick={toggle}>
+        Toggle
+      </Button>
+    </Box>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  ...Default.args,
+  when: true,
+};
--- packages/utilities/src/index.tsx
+++ packages/utilities/src/index.tsx
@@ -1,1 +1,4 @@
-export {}
+import For, { ForProps } from './components/For/For';
+import Show, { ShowProps } from './components/Show/Show';
+
+export { For, ForProps, Show, ShowProps };
