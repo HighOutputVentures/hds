86b0b4361039565da22661eec6c5661537204c86
calvo-jp <calvojp92@gmail.com>
2022-11-17 12:41:35 +0800

    update story

--- packages/tag/src/Tag.stories.tsx
+++ packages/tag/src/Tag.stories.tsx
@@ -1,9 +1,10 @@
-import { Box, extendTheme, HStack, VStack } from '@chakra-ui/react';
+import { extendTheme, VStack } from '@chakra-ui/react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import * as React from 'react';
 import { theme, ThemeProvider } from '../../hds/src';
 import AUIcon from './examples/AUIcon';
 import Tag from './Tag';
+import { omit } from './utils';
 import withTag from './withTag';
 
 export default {
@@ -11,110 +12,20 @@ export default {
   component: Tag,
 } as ComponentMeta<typeof Tag>;
 
-const Template: ComponentStory<typeof Tag> = () => {
+const Template: ComponentStory<typeof Tag> = (args) => {
   return (
     <ThemeProvider theme={extendTheme(theme, withTag())}>
-      <VStack align="start" spacing={8}>
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" />
-            <Tag size="md" label="Label" />
-            <Tag size="lg" label="Label" />
-          </HStack>
-        </Box>
-
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" indicator closable onClose={() => {}} />
-            <Tag size="md" label="Label" indicator closable onClose={() => {}} />
-            <Tag size="lg" label="Label" indicator closable onClose={() => {}} />
-          </HStack>
-        </Box>
-        <Box>
-          <HStack spacing={4}>
-            <Tag
-              size="sm"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=1'}
-              closable
-              onClose={() => {}}
-            />
-            <Tag
-              size="md"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=2'}
-              closable
-              onClose={() => {}}
-            />
-            <Tag
-              size="lg"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=3'}
-              closable
-              onClose={() => {}}
-            />
-          </HStack>
-        </Box>
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" icon={AUIcon} closable onClose={() => {}} />
-            <Tag size="md" label="Label" icon={AUIcon} closable onClose={() => {}} />
-            <Tag size="lg" label="Label" icon={AUIcon} closable onClose={() => {}} />
-          </HStack>
-        </Box>
-
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" badge badgeCount={5} />
-            <Tag size="md" label="Label" badge badgeCount={5} />
-            <Tag size="lg" label="Label" badge badgeCount={5} />
-          </HStack>
-        </Box>
-
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" checkbox checked onCheck={() => {}} />
-            <Tag size="md" label="Label" checkbox checked onCheck={() => {}} />
-            <Tag size="lg" label="Label" checkbox checked onCheck={() => {}} />
-          </HStack>
-        </Box>
-
-        <Box>
-          <HStack spacing={4}>
-            <Tag size="sm" label="Label" icon={AUIcon} checkbox checked onCheck={() => {}} />
-            <Tag size="md" label="Label" icon={AUIcon} checkbox checked onCheck={() => {}} />
-            <Tag size="lg" label="Label" icon={AUIcon} checkbox checked onCheck={() => {}} />
-          </HStack>
-        </Box>
-
-        <Box>
-          <HStack spacing={4}>
-            <Tag
-              size="sm"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=1'}
-              checkbox
-              checked
-              onCheck={() => {}}
-            />
-            <Tag
-              size="md"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=2'}
-              checkbox
-              checked
-              onCheck={() => {}}
-            />
-            <Tag
-              size="lg"
-              label="Label"
-              avatar={'https://i.pravatar.cc/250?u=3'}
-              checkbox
-              checked
-              onCheck={() => {}}
-            />
-          </HStack>
-        </Box>
+      <VStack align="start" spacing={4}>
+        {/* @ts-ignore */}
+        <Tag label="" {...omit(args, 'icon', 'avatar', 'checkbox', 'indicator', 'closable')} />
+        {/* @ts-ignore */}
+        <Tag label="" {...omit(args, 'icon', 'avatar', 'checkbox', 'indicator')} />
+        {/* @ts-ignore */}
+        <Tag label="" {...omit(args, 'icon', 'avatar', 'checkbox')} />
+        {/* @ts-ignore */}
+        <Tag label="" {...omit(args, 'icon', 'indicator')} />
+        {/* @ts-ignore */}
+        <Tag label="" {...omit(args, 'avatar', 'indicator')} />
       </VStack>
     </ThemeProvider>
   );
@@ -122,4 +33,15 @@ const Template: ComponentStory<typeof Tag> = () => {
 
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  size: 'lg',
+  label: 'Label',
+  closable: true,
+  onClose: () => {},
+  avatar: 'https://i.pravatar.cc/25',
+  indicator: true,
+  checkbox: true,
+  icon: AUIcon,
+  onCheck: () => {},
+  ...Default.args,
+};
