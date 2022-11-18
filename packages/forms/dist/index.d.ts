413aadeaa1c8c9150a9333e7bf116f0cfc1a4490
Niel Berongoy <niel_berongoy@yahoo.com>
2022-11-18 11:52:26 +0800

    chore: remove dist

--- packages/forms/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import FormContainer, { FormContainerPartProps, FormContainerProps } from './FormContainer/FormContainer';
-import InputField, { InputFieldPartProps, InputFieldProps } from './InputField/InputField';
-import PasswordInputField, { PasswordInputFieldProps } from './PasswordInputField/PasswordInputField';
-import PinInputField, { PinInputFieldPartProps, PinInputFieldProps } from './PinInputField/PinInputField';
-import SelectField, { SelectFieldProps } from './SelectField/SelectField';
-import TextAreaField, { TextAreaFieldProps } from './TextareaField/TextareaField';
-export { FormContainer, FormContainerProps, FormContainerPartProps, InputField, InputFieldProps, InputFieldPartProps, PasswordInputField, PasswordInputFieldProps, PinInputField, PinInputFieldPartProps, PinInputFieldProps, SelectField, SelectFieldProps, TextAreaField, TextAreaFieldProps, };
