3fd684ce7f47082355630fa4cfa2e1b23a8bf345
calvo-jp <calvojp92@gmail.com>
2022-11-21 13:37:32 +0800

    added test for withSwitch

--- packages/switch/src/withSwitch.spec.ts
+++ packages/switch/src/withSwitch.spec.ts
@@ -1,1 +1,13 @@
-describe("withSwitch", () => {});
+import withSwitch from "./withSwitch";
+
+describe("withSwitch", () => {
+  it("Should return theme for 'Switch'", () => {
+    expect(withSwitch()).toEqual(
+      expect.objectContaining({
+        components: {
+          Switch: expect.any(Object),
+        },
+      }),
+    );
+  });
+});
