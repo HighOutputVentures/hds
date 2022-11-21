519aac3f68d6071baa578c2acdb1b83c0e6ce2c5
calvo-jp <calvojp92@gmail.com>
2022-11-21 16:15:38 +0800

    refactor pagination

--- packages/pagination/src/components/Pagination.test.tsx
+++ packages/pagination/src/components/Pagination.test.tsx
@@ -1,5 +1,4 @@
-import '@testing-library/react/dont-cleanup-after-each';
-
+import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import * as React from 'react';
 import Pagination from './Pagination';
@@ -8,7 +7,7 @@ const handlePageChange = jest.fn();
 const handleSizeChange = jest.fn();
 
 describe('Pagination', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     render(
       <Pagination
         page={2}
@@ -23,8 +22,8 @@ describe('Pagination', () => {
     );
   });
 
-  it('Should be able to change page size', async () => {
-    const dropdown = await screen.getByTestId(':r0:-pagination.dropdown');
+  it('Should be able to change page size', () => {
+    const dropdown = screen.getByRole('combobox', { name: 'Change page size' });
 
     fireEvent.change(dropdown, {
       target: {
@@ -35,14 +34,14 @@ describe('Pagination', () => {
     expect(handleSizeChange).toBeCalledWith(expect.any(Number));
   });
 
-  it('Should be able to go to previous page', async () => {
-    const prevBtn = await screen.getByTestId(':r0:-pagination.controls.prev');
+  it('Should be able to go to previous page', () => {
+    const prevBtn = screen.getByRole('button', { name: 'Go to previous page' });
     fireEvent.click(prevBtn);
     expect(handlePageChange).toBeCalledWith(expect.any(Number));
   });
 
-  it('Should be able to go to next page', async () => {
-    const nextBtn = await screen.getByTestId(':r0:-pagination.controls.next');
+  it('Should be able to go to next page', () => {
+    const nextBtn = screen.getByRole('button', { name: 'Go to next page' });
     fireEvent.click(nextBtn);
     expect(handlePageChange).toBeCalledWith(expect.any(Number));
   });
