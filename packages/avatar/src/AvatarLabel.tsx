f504690f55dd7922417a936b14fda743c4248a2d
calvo-jp <calvojp92@gmail.com>
2022-11-21 20:02:54 +0800

    avatar package

---
+++ packages/avatar/src/AvatarLabel.tsx
@@ -0,0 +1,85 @@
+import { Box, Heading, SystemStyleObject, Text, useMultiStyleConfig } from "@chakra-ui/react";
+import * as React from "react";
+import { PropsWithChildren } from "react";
+import { AvatarBaseProps } from "./Avatar";
+import { Badgeable, Clickable, FreeSize } from "./types";
+import { omit, pick } from "./utils";
+
+type Size = "sm" | "md" | "lg" | "xl";
+
+type AvatarLabelBaseProps = {
+  size?: Size;
+  fallback?: boolean;
+  supportText?: string;
+};
+
+// prettier-ignore
+export type AvatarLabelProps = 
+  SystemStyleObject &
+  FreeSize<AvatarBaseProps> &
+  AvatarLabelBaseProps &
+  Badgeable &
+  Clickable;
+
+export default function AvatarLabel(props: PropsWithChildren<AvatarLabelProps>) {
+  const {
+    src,
+    name,
+    size = "md",
+    fallback = true,
+    children,
+    supportText,
+    online,
+    onlineIndicator,
+    ...others
+  } = props;
+
+  const styles = useMultiStyleConfig("AvatarLabel", { size, variant: "hds" });
+
+  if (!React.isValidElement(children)) {
+    console.error("'AvatarLabel' only accepts a single 'Avatar' child");
+
+    return null;
+  }
+
+  return (
+    <Box
+      sx={{
+        ...omit(
+          others,
+          "badge",
+          "clickable",
+          "verified",
+          /* @ts-ignore "This uses type guards here" */
+          "onClick",
+          "badgeIcon",
+        ),
+        ...styles.container,
+      }}
+    >
+      <Box sx={styles.avatar}>
+        {React.cloneElement<any>(children, {
+          src,
+          name: fallback ? name : undefined,
+          size,
+          online,
+          onlineIndicator,
+          ...pick(
+            others,
+            "badge",
+            "verified",
+            "clickable",
+            /* @ts-ignore "This uses type guards here" */
+            "onClick",
+            "badgeIcon",
+          ),
+        })}
+      </Box>
+
+      <Box sx={styles.label}>
+        <Heading sx={styles.heading}>{name}</Heading>
+        <Text sx={styles.supportText}>{supportText}</Text>
+      </Box>
+    </Box>
+  );
+}
