51f80c96ddefc3f7162bee11f9409cadd0dc38e6
calvo-jp <calvojp92@gmail.com>
2022-11-23 10:26:10 +0800

    update tsconfig

---
+++ packages/autocomplete-input/dist/index.d.ts
@@ -0,0 +1,2 @@
+import AutoCompleteInput, { AutoCompleteInputFieldPartProps, AutoCompleteInputFieldProps } from './AutoCompleteInput/AutoCompleteInput';
+export { AutoCompleteInput, AutoCompleteInputFieldProps, AutoCompleteInputFieldPartProps, };
---
+++ packages/tag/dist/utils.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { SpacerProps } from '@chakra-ui/react';
+export declare function omit<T extends Record<string, any>, K extends (keyof T)[]>(subject: T, ...keys: K): Omit<T, K[number]>;
+export declare function space(when: boolean, props: SpacerProps): JSX.Element | null;
