a4887252112aba5e211884a49350dbdc92f29081
calvo-jp <calvojp92@gmail.com>
2022-11-25 08:38:46 +0800

    add tests

--- packages/checkbox-group/src/CheckboxGroup.spec.tsx
+++ packages/checkbox-group/src/CheckboxGroup.spec.tsx
@@ -1,4 +1,8 @@
+import { Box } from "@chakra-ui/react";
 import "@testing-library/jest-dom";
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import * as React from "react";
+import CheckboxGroup from "./CheckboxGroup";
 
 const items = [
   { id: 1, name: "Mary" },
@@ -9,10 +13,125 @@ const items = [
 ];
 
 describe("CheckboxGroup", () => {
-  it.todo("Should render children properly");
-  it.todo("Should handle (de)selecting items");
-  it.todo("Should be able to pass default value");
-  it.todo("Should be able to disable item");
+  it("Should render children properly", () => {
+    const { queryAllByRole } = render(
+      <CheckboxGroup items={items} value={items[0]} onChange={jest.fn()}>
+        {({ item, getProps }) => {
+          const { container } = getProps();
+
+          return (
+            <Box key={item.id} {...container}>
+              {item.name}
+            </Box>
+          );
+        }}
+      </CheckboxGroup>,
+    );
+
+    expect(queryAllByRole("checkbox", { name: /select item \d/i })).toHaveLength(items.length);
+  });
+
+  it("Should handle (de)selecting items", async () => {
+    const handleChange = jest.fn();
+
+    const { getAllByRole } = render(
+      <CheckboxGroup
+        items={items}
+        value={[items[0]]}
+        onChange={handleChange}
+        multiple
+        compareFn={({ id }) => id}
+      >
+        {({ item, getProps }) => {
+          const { container } = getProps();
+
+          return (
+            <Box key={item.id} {...container}>
+              {item.name}
+            </Box>
+          );
+        }}
+      </CheckboxGroup>,
+    );
+
+    fireEvent.click(getAllByRole("checkbox", { name: /select item \d/i })[0]);
+
+    await waitFor(() => {
+      expect(handleChange).toHaveBeenCalledWith([]);
+    });
+
+    // value will not actually change
+    // since CheckboxGroup is controlled
+
+    // fireEvent.click(getAllByRole("checkbox", { name: /select item \d/i })[1]);
+
+    // await waitFor(() => {
+    //   expect(handleChange).toHaveBeenCalledWith(expect.arrayContaining([items[1]]));
+    // });
+  });
+
+  it("Should be able to pass default value", () => {
+    const { queryAllByRole } = render(
+      <CheckboxGroup items={items} value={[items[0], items[1]]} onChange={jest.fn()} multiple>
+        {({ item, getProps }) => {
+          const { container } = getProps();
+
+          return (
+            <Box key={item.id} {...container}>
+              {item.name}
+            </Box>
+          );
+        }}
+      </CheckboxGroup>,
+    );
+
+    expect(queryAllByRole("checkbox", { name: /select item \d/i, checked: true })).toHaveLength(2);
+  });
+
+  it("Should be able to disable item", () => {
+    const { getAllByRole } = render(
+      <CheckboxGroup items={items} value={[items[0], items[1]]} onChange={jest.fn()} multiple>
+        {({ item, index, getProps }) => {
+          const { container } = getProps({ disabled: [0, 1].includes(index) });
+
+          return (
+            <Box key={item.id} {...container}>
+              {item.name}
+            </Box>
+          );
+        }}
+      </CheckboxGroup>,
+    );
+
+    const checkboxes = getAllByRole("checkbox", { name: /select item \d/i });
+
+    expect(checkboxes[0]).toHaveAttribute("aria-disabled", "true");
+    expect(checkboxes[1]).toHaveAttribute("aria-disabled", "true");
+  });
+
   it.todo("Should be able to toggle multiple");
-  it.todo("Should call onChange when selected items change");
+
+  it("Should call onChange when selected items change", async () => {
+    const handleChange = jest.fn();
+
+    const { getAllByRole } = render(
+      <CheckboxGroup items={items} value={[items[0], items[1]]} onChange={handleChange} multiple>
+        {({ item, getProps }) => {
+          const { container } = getProps();
+
+          return (
+            <Box key={item.id} {...container}>
+              {item.name}
+            </Box>
+          );
+        }}
+      </CheckboxGroup>,
+    );
+
+    fireEvent.click(getAllByRole("checkbox", { name: /select item \d/i })[0]);
+
+    await waitFor(() => {
+      expect(handleChange).toHaveBeenCalledWith(expect.arrayContaining([items[1]]));
+    });
+  });
 });
