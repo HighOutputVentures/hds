7b519c2d28b1fb609b22d494c177fd3965f2a3a4
calvo-jp <calvojp92@gmail.com>
2022-11-28 18:02:57 +0800

    fix incorrect import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import "../globals/overrides.css";
+import "../styles/globals.css";
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
