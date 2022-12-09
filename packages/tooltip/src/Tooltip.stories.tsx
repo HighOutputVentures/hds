596bdabee9f8fc843773bb6b812e9e6ef9566ac7
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2022-12-09 16:40:48 +0800

    tooltip stories

---
+++ packages/tooltip/src/Tooltip.stories.tsx
@@ -0,0 +1,37 @@
+import { Flex, Icon } from '@chakra-ui/react';
+import ThemeProvider from '@highoutput/hds/src/components/ThemeProvider';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import React from 'react';
+
+import Tooltip from './Tooltip';
+
+export default {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+} as ComponentMeta<typeof Tooltip>;
+
+const Template: ComponentStory<typeof Tooltip> = (props) => {
+  return (
+    <ThemeProvider>
+      <Flex
+        gap={'50px'}
+        flexDir="column"
+        width="full"
+        justify={'center'}
+        align={'center'}
+        height={'400px'}
+      >
+        <Tooltip {...props} label="This is a tooltip" isOpen={true}>
+          <Icon />
+        </Tooltip>
+      </Flex>
+    </ThemeProvider>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  ...Default.args,
+};
