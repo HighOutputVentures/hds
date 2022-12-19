bf9617d9032ac8bcee41537d296f7387877ae183
calvo-jp <calvojp92@gmail.com>
2022-12-19 14:13:51 +0800

    added icons

---
+++ packages/table/src/icons/ArrowDownIcon.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+export default function ArrowDownIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M7.99992 3.33325V12.6666M7.99992 12.6666L12.6666 7.99992M7.99992 12.6666L3.33325 7.99992"
+        stroke="currentColor"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
