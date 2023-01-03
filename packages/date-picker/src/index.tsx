9fb8349a2b53be477d6300b626c34598a1379fdf
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2023-01-03 13:55:29 +0800

    datepicker docs and configs

---
+++ packages/date-picker/src/index.tsx
@@ -0,0 +1,22 @@
+import {
+  dateInfoProps,
+  DatePicker,
+  DatePickerProps,
+  IndividualDateProps,
+  IndividualMainDateProps,
+  IndividualNextDateProps,
+  IndividualPrevDateProps,
+} from './DatePicker';
+import { DatePickerDropdownProps } from './DatePickerDropdown';
+import { DatePickerModalProps } from './DatePickerModal';
+export {
+  DatePicker,
+  DatePickerProps,
+  DatePickerDropdownProps,
+  DatePickerModalProps,
+  IndividualDateProps,
+  IndividualMainDateProps,
+  IndividualNextDateProps,
+  IndividualPrevDateProps,
+  dateInfoProps,
+};
