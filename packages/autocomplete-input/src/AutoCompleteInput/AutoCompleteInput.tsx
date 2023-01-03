b3245a13d634b05a1b0ad890348c40624eaf55c8
calvo-jp <calvojp92@gmail.com>
2023-01-03 14:07:51 +0800

    revert ts-ignore

--- packages/autocomplete-input/src/AutoCompleteInput/AutoCompleteInput.tsx
+++ packages/autocomplete-input/src/AutoCompleteInput/AutoCompleteInput.tsx
@@ -1,5 +1,6 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 import { Avatar, HStack, Icon } from '@chakra-ui/react';
+// @ts-ignore "Bug 🐜"
 import { FormContainer, FormContainerProps } from '@highoutput/hds-forms';
 import {
   chakraComponents,
