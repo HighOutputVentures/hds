baf65878306a8b872363b0486c291349496ddc23
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2023-01-20 11:30:16 +0800

    chore: remove tabs in hds core moved under hds-tab

--- packages/tab/src/index.ts
+++ packages/tab/src/index.ts
@@ -1,4 +1,27 @@
+import {
+  TabList,
+  TabListProps,
+  TabPanel,
+  TabPanelProps,
+  TabPanels,
+  TabPanelsProps,
+  Tabs,
+  TabsProps,
+} from '@chakra-ui/react';
 import ButtonTabs, { ButtonTabsProps } from './ButtonTabs';
 import Tab, { TabProps } from './tab';
 
-export { ButtonTabs, ButtonTabsProps, Tab, TabProps };
+export {
+  ButtonTabs,
+  ButtonTabsProps,
+  Tab,
+  TabProps,
+  TabList,
+  TabListProps,
+  TabPanel,
+  TabPanelProps,
+  TabPanels,
+  TabPanelsProps,
+  Tabs,
+  TabsProps,
+};
