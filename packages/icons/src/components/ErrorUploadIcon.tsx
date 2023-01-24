bbf7c7f9b8c0ce7c5277da1093f1c80e77daee76
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2023-01-24 16:20:15 +0800

    chore: Convert all stroke-width to strokeWidth

--- packages/icons/src/components/ErrorUploadIcon.tsx
+++ packages/icons/src/components/ErrorUploadIcon.tsx
@@ -7,9 +7,9 @@ const ErrorUploadIcon: FC<IconProps> = (props) => (
     <path
       d="M15.3333 20.6667L18 18M18 18L20.6667 20.6667M18 18V24M23.3333 21.1619C24.1477 20.4894 24.6667 19.472 24.6667 18.3333C24.6667 16.3083 23.025 14.6667 21 14.6667C20.8543 14.6667 20.718 14.5907 20.6441 14.4652C19.7747 12.9899 18.1696 12 16.3333 12C13.5719 12 11.3333 14.2386 11.3333 17C11.3333 18.3774 11.8903 19.6247 12.7913 20.529"
       stroke="#DC180C"
-      stroke-width="1.33333"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.33333"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <rect
       x="2"
@@ -18,7 +18,7 @@ const ErrorUploadIcon: FC<IconProps> = (props) => (
       height="32"
       rx="16"
       stroke="#FCEAE8"
-      stroke-width="4"
+      strokeWidth="4"
     />
   </Icon>
 );
