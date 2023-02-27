fa8c588f487c730ca57ba647d6270f3b25eb6338
John Christopher Go <go.jchristopher@gmail.com>
2023-02-27 16:47:57 +0800

    initial separation accordion and alert

--- packages/hds/src/components/ThemeProvider.tsx
+++ packages/hds/src/components/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, ChakraProviderProps } from '@chakra-ui/react';
+import { ChakraBaseProvider, ChakraProviderProps } from '@chakra-ui/react';
 import React, { FC } from 'react';
 
 import { extendTheme } from '../utils/theme.utils';
@@ -8,10 +8,10 @@ export interface ThemeProviderProps extends ChakraProviderProps {}
 
 const ThemeProvider: FC<ThemeProviderProps> = ({ children, ...props }) => {
   return (
-    <ChakraProvider {...props} theme={extendTheme(props.theme)}>
+    <ChakraBaseProvider {...props} theme={extendTheme(props.theme)}>
       <Fonts />
       {children}
-    </ChakraProvider>
+    </ChakraBaseProvider>
   );
 };
 
