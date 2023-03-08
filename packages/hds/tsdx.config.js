91dec4bc5b7791af59a9f34f3bbe9b1241776777
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2023-03-08 14:49:21 +0800

    chore: minor changes

--- packages/hds/tsdx.config.js
+++ packages/hds/tsdx.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   rollup(config, options) {
-    config.plugins = [...config.plugins];
+    const outputDir = process.cwd() + '/dist/';
+    const extension = '.' + config.output.file.split('.').slice(1).join('.');
+    let filename = config.input.split('src/')[1];
+
+    filename = filename.split('.')[0];
+
+    const fileName = config.input === './src/index.tsx' ? 'hds' : filename;
+    config.output.file = outputDir + fileName + extension;
+
+    config.output.name = filename.replace('/', '__');
 
     return config;
   },
