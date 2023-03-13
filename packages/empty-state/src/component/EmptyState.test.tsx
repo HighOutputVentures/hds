333f9555eeda57b8e7d4cee3470ec65ef172d293
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2023-03-13 12:01:41 +0800

    feat: Empty state

---
+++ packages/empty-state/src/component/EmptyState.test.tsx
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import React from 'react';
+import EmptyState from './EmptyState';
+
+describe('EmptyState', () => {
+  it('should render EmptyState', () => {
+    const { container } = render(
+      <EmptyState
+        header={`No projects found`}
+        subHeader="Your search “Landing page design” did not match any projects. Please try again."
+        primaryLabel="Changelog"
+        secondaryLabel="Dismiss"
+      />
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render EmptyState', () => {
+    const { getByText } = render(
+      <EmptyState
+        header={`No projects found`}
+        subHeader="Your search “Landing page design” did not match any projects. Please try again."
+        primaryLabel="Changelog"
+        secondaryLabel="Dismiss"
+      />
+    );
+
+    expect(getByText(`No projects found`)).toBeInTheDocument();
+    expect(
+      getByText(
+        `Your search “Landing page design” did not match any projects. Please try again.`
+      )
+    ).toBeInTheDocument();
+  });
+});
---
+++ packages/empty-state/src/component/EmptyState.tsx
@@ -0,0 +1,69 @@
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, Text, Flex, Button } from '@chakra-ui/react';
+import React, { FC, ReactElement } from 'react';
+
+export interface EmptyStateProps {
+  header: string;
+  subHeader: string;
+  icon?: ReactElement;
+  primaryLabel: string;
+  secondaryLabel: string;
+  onClear?: () => void;
+  onSubmit?: () => void;
+}
+
+const EmptyState: FC<EmptyStateProps> = ({
+  header,
+  subHeader,
+  icon,
+  primaryLabel,
+  secondaryLabel,
+  onClear,
+  onSubmit,
+}) => (
+  <Box>
+    <Flex
+      direction="column"
+      w={{ base: 'auto', lg: '48rem' }}
+      justifyContent="center"
+      p="24px"
+      textAlign="center"
+      alignItems="center"
+    >
+      {icon && (
+        <Box my="1rem" bgColor="">
+          {icon}
+        </Box>
+      )}
+      <Text
+        fontWeight={500}
+        size={{ base: 'label-sm-default', lg: 'label-md-default' }}
+      >
+        {header}
+      </Text>
+      <Text
+        w={{ lg: 352 }}
+        color="gray.500"
+        size={{ base: 'label-sm-default', lg: 'paragraphs-md-default' }}
+        my="0.5rem"
+      >
+        {subHeader}
+      </Text>
+
+      <Flex gap={4} mt="1rem">
+        {secondaryLabel && (
+          <Button onClick={onClear} variant="outline">
+            {secondaryLabel}
+          </Button>
+        )}
+        {primaryLabel && (
+          <Button leftIcon={<AddIcon />} onClick={onSubmit}>
+            {primaryLabel}
+          </Button>
+        )}
+      </Flex>
+    </Flex>
+  </Box>
+);
+
+export default EmptyState;
