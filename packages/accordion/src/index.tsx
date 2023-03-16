b034264a5350d84c143b4376234209f29b35c9c9
John Christopher Go <go.jchristopher@gmail.com>
2023-03-16 11:33:12 +0800

    feat: separated accordion as a package

---
+++ packages/accordion/src/index.tsx
@@ -0,0 +1,13 @@
+export {
+  Accordion,
+  AccordionButton,
+  AccordionButtonProps,
+  AccordionIcon,
+  AccordionIconProps,
+  AccordionItem,
+  AccordionItemProps,
+  AccordionPanel,
+  AccordionPanelProps,
+  AccordionProps,
+  AccordionProvider,
+} from '@chakra-ui/react';
