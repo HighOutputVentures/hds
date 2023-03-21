d7ac504cc21df716578d161c5eec18dea96e7e24
John Christopher Go <go.jchristopher@gmail.com>
2023-03-21 15:38:44 +0800

    add drawer package

---
+++ packages/drawer/src/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+} from '@chakra-ui/react';
+
+export {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+};
