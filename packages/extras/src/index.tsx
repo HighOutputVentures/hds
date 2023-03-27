07e13a6c33bb6cd763a0995d374d4afa369c13f5
John Christopher Go <go.jchristopher@gmail.com>
2023-03-27 12:19:46 +0800

    feat: add extras package

---
+++ packages/extras/src/index.tsx
@@ -0,0 +1,11 @@
+import {
+  Slide,
+  SlideFade,
+  SlideDirection,
+  Spacer,
+  Divider,
+  Fade,
+  ScaleFade,
+} from '@chakra-ui/react';
+
+export { Slide, SlideFade, SlideDirection, Spacer, Divider, Fade, ScaleFade };
