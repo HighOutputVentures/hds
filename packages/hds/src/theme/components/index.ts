b7c76bb36479e14ad93bd4a9176ba78cd8942f30
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2023-04-13 18:04:49 +0800

    chore: moved checkbox style config to checbox component

--- packages/hds/src/theme/components/index.ts
+++ packages/hds/src/theme/components/index.ts
@@ -1,10 +1,8 @@
-import Checkbox from './checkbox';
 import Drawer from './drawer';
 import Heading from './heading';
 import Text from './text';
 
 const components = {
-  Checkbox,
   Text,
   Heading,
   Drawer,
